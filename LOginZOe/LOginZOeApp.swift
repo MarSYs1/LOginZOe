2611549e6bd0481e9a69ab672e5c686e607a0e43
Mauro de Oliveira Andre <mauroolivere@hotmail.com>
2022-04-11 20:32:20 +0100

    Initial Commit

---
+++ LOginZOe/LOginZOeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOginZOeApp.swift
+//  LOginZOe
+//
+//  Created by Mauro Sys One on 4/11/22.
+//
+
+import SwiftUI
+
+@main
+struct LOginZOeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
